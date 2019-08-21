afb091d4a018f758eb2b5530bdb60942c0b2acf0
shay8el <44734542+shay8el@users.noreply.github.com>
2019-08-21 20:56:42 +0300

    cleanups

--- Server/vote.py
+++ Server/vote.py
@@ -1,10 +1,8 @@
-from base64 import b64encode, b64decode
+from base64 import b64encode
 
-from flask import g
 
 from DAL.app_context_db import get_db_connector
-from mock_response import get_candidates as mock_get_candidates
-import json
+
 
 
 def get_candidates():
